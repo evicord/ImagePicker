de3c6608e14cc6cdee7b022d6756f31775efecfd
酒枫子 <maple_leaf_7@msn.com>
2016-01-13 14:02:02 +0800

    路径bug

--- src/android/common/ExtraKey.java
+++ src/android/common/ExtraKey.java
@@ -4,5 +4,5 @@ public class ExtraKey {
 	public static final String LOCAL_FOLDER_NAME = "local_folder_name";//跳转到相册页的文件夹名称
 	public static int IMAGE_CHOSE_COUNT = 10;//跳转到相册页的文件夹名称
 	public static int IMAGE_MAX_SIZE = 2048;//取出图片最大边长
-	public static final String COMPRESS_FODER_NAME = "PanArt";//跳转到相册页的文件夹名称
+	public static final String COMPRESS_FODER_NAME = ".PanArt";//跳转到相册页的文件夹名称
 }
